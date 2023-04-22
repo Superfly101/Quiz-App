03089e197290eca12496dd4dbd39c8f2b6f8a0ee
Daniel <danielukoha101@gmail.com>
2023-04-22 11:44:03 +0100

    update loading spinner

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { Box, Spinner } from "@chakra-ui/react";
 
 const LoadingSpinner = () => {
   return (
-    <Box mt="7rem">
+    <Box mt="7rem" textAlign="center">
       <Spinner speed="0.5s" thickness="4px" color="blue.500" size="xl" />;
     </Box>
   );
